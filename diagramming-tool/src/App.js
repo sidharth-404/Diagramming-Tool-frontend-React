572c5761f9220f7f6de3a75b796eb4eabf5c4f3e
Ajanya Alooparamban Thilakan(UST <249417@ust.com>
2024-03-22 15:48:28 +0530

    I completed my HomePage

--- diagramming-tool/src/App.js
+++ diagramming-tool/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HomePage from './Component/HomePage/HomePage';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>HAi</h1>
+       <HomePage/>
       </header>
     </div>
   );
