14aa8ee47b8295869f5e93003b27f5d2be1c94f6
Abin James(UST <248726@ust.com>
2024-03-22 10:51:48 +0530

    last

--- diagramming-tool/src/App.js
+++ diagramming-tool/src/App.js
@@ -1,13 +1,37 @@
+import React, { useState } from 'react';
 import './App.css';
+import MsgComponent from './Component/ConfirmMsg/MsgComponent';
+const App = () => {
 
-function App() {
+  const [showModal, setShowModal] = useState(false);
+
+const openModal = () => {
+  setShowModal(true);
+};
+
+const closeModal = () => {
+  setShowModal(false);
+  alert("hai");
+};
+const cancelModel=()=>{
+  setShowModal(false);
+}
+const msg="hai hoi"
+  
   return (
     <div className="App">
       <header className="App-header">
-       <h1>HAi</h1>
+        <h1>HAi</h1>
+        <button className="btn btn-primary" onClick={openModal}>
+Open Modal
+</button>
+      <MsgComponent showModal={showModal} closeModal={closeModal} cancelModel={cancelModel} msg={msg} /> 
+
       </header>
+
+      
     </div>
   );
-}
+};
 
 export default App;
