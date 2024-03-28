6b22aa1486b19faea91526c53d8ac17ee46203b2
Feril John <248832@ust.com>
2024-03-28 14:00:05 +0530

    adding setupTest

--- diagramming-tool/src/setupTests.js
+++ diagramming-tool/src/setupTests.js
@@ -1,2 +1,15 @@
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
+class IntersectionObserver {
+    constructor() {}
+     observe() {    
+    }
+    disconnect() {
+    }
+  }
+  global.IntersectionObserver = IntersectionObserver;
+  
+
+
+
+
