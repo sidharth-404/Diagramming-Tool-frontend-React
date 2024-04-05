4042a0b8a129f56da8445bc233e7fc56fbc641d3
Feril John <248832@ust.com>
2024-04-05 19:57:17 +0530

    Updated

--- diagramming-tool/src/Component/Dashboard/Canvas.jsx
+++ diagramming-tool/src/Component/Dashboard/Canvas.jsx
@@ -1,12 +1,25 @@
 import React, { useState, useRef, useEffect, useCallback } from "react";
 import "./Canvas.css";
 import { Rectangle, Circle, Square, Diamond } from "./NewShapes";
+import { IoArrowUndo, IoArrowRedo } from "react-icons/io5";
+import { MdDeleteForever, MdFileOpen } from "react-icons/md";
+import { TfiSave } from "react-icons/tfi";
 import ShapeTypes from "./ShapeTypes";
 
+
+
 const CanvasComponent = () => {
-  const [selectedShape, setSelectedShape] = useState(null);
+  
+  const [selectedShapeId, setSelectedShapeId] = useState(null);
+  const [selectedShape,setSelectedShape] = useState(null);
+  const [selectedButton, setSelectedButton] = useState(null);
+  const [hoveredButton] = useState("");
   const canvasRef = useRef(null);
   const [shapes, setShapes] = useState([]);
+  const [dragging, setDragging] = useState(false);
+  const [startPoint, setStartPoint] = useState({ x: 0, y: 0 });
+  const [endPoint, setEndPoint] = useState({ x: 0, y: 0 });
+  const [lines, setLines] = useState([]);
   const isDrawing = useRef(false);
   const draggedShape = useRef(null);
   const dragOffset = useRef({ x: 0, y: 0 });
@@ -101,12 +114,195 @@ const CanvasComponent = () => {
         ctx.fillText(text, centerX, centerY);
       }
     });
-  }, [shapes, textInputs]);
+  
+  
+    lines.forEach((line) => {
+     
+      ctx.beginPath();
+      ctx.moveTo(line.startPoint.x, line.startPoint.y);
+      ctx.lineTo(line.endPoint.x, line.endPoint.y);
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    });
 
-
+    if (selectedShapeId) {
+      const selectedShape = shapes.find((shape) => shape.id === selectedShapeId);
+      if (selectedShape) {
+        drawSelectionPoints(ctx, selectedShape);
+      }
+    }if (dragging) {
+      ctx.beginPath();
+      ctx.moveTo(startPoint.x, startPoint.y);
+      ctx.lineTo(endPoint.x, endPoint.y);
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+  }, [shapes, selectedShapeId, dragging, startPoint, endPoint,lines,textInputs]);
+  
   useEffect(() => {
     draw();
   }, [draw]);
+  
+  
+
+
+  const drawSelectionPoints = (ctx, shape) => {
+    const pointSize = 5;
+    const halfPointSize = pointSize / 2;
+    ctx.fillStyle = "blue";
+
+    switch (shape.type) {
+      case ShapeTypes.RECTANGLE:
+        ctx.fillRect(shape.x - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.width * 2 - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x - halfPointSize, shape.y + shape.height - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.width * 2 - halfPointSize, shape.y + shape.height - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.width - halfPointSize, shape.y - halfPointSize, pointSize, pointSize); 
+        ctx.fillRect(shape.x - halfPointSize, shape.y + shape.height / 2 - halfPointSize, pointSize, pointSize); 
+        ctx.fillRect(shape.x + shape.width * 2 - halfPointSize, shape.y + shape.height / 2 - halfPointSize, pointSize, pointSize); 
+        ctx.fillRect(shape.x + shape.width - halfPointSize, shape.y + shape.height - halfPointSize, pointSize, pointSize);
+        break;
+      case ShapeTypes.CIRCLE:
+        ctx.fillRect(shape.x - halfPointSize, shape.y - shape.radius - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x - halfPointSize, shape.y + shape.radius - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x - shape.radius - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.radius - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x - shape.radius / Math.sqrt(2) - halfPointSize, shape.y - shape.radius / Math.sqrt(2) - halfPointSize, pointSize, pointSize); 
+        ctx.fillRect(shape.x + shape.radius / Math.sqrt(2) - halfPointSize, shape.y - shape.radius / Math.sqrt(2) - halfPointSize, pointSize, pointSize); 
+        ctx.fillRect(shape.x - shape.radius / Math.sqrt(2) - halfPointSize, shape.y + shape.radius / Math.sqrt(2) - halfPointSize, pointSize, pointSize); 
+        ctx.fillRect(shape.x + shape.radius / Math.sqrt(2) - halfPointSize, shape.y + shape.radius / Math.sqrt(2) - halfPointSize, pointSize, pointSize);
+        break;
+      case ShapeTypes.SQUARE:
+        ctx.fillRect(shape.x - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.size - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x - halfPointSize, shape.y + shape.size - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.size - halfPointSize, shape.y + shape.size - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.size / 2 - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.size / 2 - halfPointSize, shape.y + shape.size - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x - halfPointSize, shape.y + shape.size / 2 - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.size - halfPointSize, shape.y + shape.size / 2 - halfPointSize, pointSize, pointSize);
+        break;
+      case ShapeTypes.DIAMOND:
+        ctx.fillRect(shape.x - halfPointSize, shape.y + shape.height / 2 - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.width - halfPointSize, shape.y + shape.height / 2 - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.width / 2 - halfPointSize, shape.y - halfPointSize, pointSize, pointSize);
+        ctx.fillRect(shape.x + shape.width / 2 - halfPointSize, shape.y + shape.height - halfPointSize, pointSize, pointSize);
+        break;
+      default:
+        break;
+    }
+  };
+  const handleCanvasMouseDown = (event) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+  
+    const offsetX = event.nativeEvent.offsetX;
+    const offsetY = event.nativeEvent.offsetY;
+  
+    
+    if (selectedShapeId) {
+      setDragging(true);
+      setStartPoint({ x: offsetX, y: offsetY });
+      setEndPoint({ x: offsetX, y: offsetY });
+    }
+  };
+  
+  const handleCanvasMouseMove = (event) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+  
+    const offsetX = event.nativeEvent.offsetX;
+    const offsetY = event.nativeEvent.offsetY;
+  
+  
+    if (dragging) {
+      setEndPoint({ x: offsetX, y: offsetY });
+    }
+  };
+  
+  const handleCanvasMouseUp = (event) => {
+    if (dragging) {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+  
+      const offsetX = event.nativeEvent.offsetX;
+      const offsetY = event.nativeEvent.offsetY;
+      setLines([...lines, { startPoint, endPoint }]);
+   
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.beginPath();
+        ctx.moveTo(startPoint.x, startPoint.y);
+        ctx.lineTo(offsetX, offsetY);
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+      }
+  
+      setDragging(false);
+    }
+  };
+  
+  
+  const handleCanvasClick = (event) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const offsetX = event.nativeEvent.offsetX;
+    const offsetY = event.nativeEvent.offsetY;
+
+    let clickedShapeId = null;
+
+    shapes.forEach((shape) => {
+      switch (shape.type) {
+        case ShapeTypes.RECTANGLE:
+          if (
+            offsetX >= shape.x &&
+            offsetX <= shape.x + shape.width * 2 &&
+            offsetY >= shape.y &&
+            offsetY <= shape.y + shape.height
+          ) {
+            clickedShapeId = shape.id;
+          }
+          break;
+        case ShapeTypes.CIRCLE:
+          const distance = Math.sqrt(Math.pow(offsetX - shape.x, 2) + Math.pow(offsetY - shape.y, 2));
+          if (distance <= shape.radius) {
+            clickedShapeId = shape.id;
+          }
+          break;
+        case ShapeTypes.SQUARE:
+          if (
+            offsetX >= shape.x &&
+            offsetX <= shape.x + shape.size &&
+            offsetY >= shape.y &&
+            offsetY <= shape.y + shape.size
+          ) {
+            clickedShapeId = shape.id;
+          }
+          break;
+        case ShapeTypes.DIAMOND:
+          if (isPointInsideDiamond(offsetX, offsetY, shape)) {
+            clickedShapeId = shape.id;
+          }
+          break;
+        default:
+          break;
+      }
+    });
+
+    setSelectedShapeId((prevId) =>
+      prevId === clickedShapeId ? null : clickedShapeId
+    );
+  };
+
+  const isPointInsideDiamond = (pointX, pointY, diamond) => {
+    const deltaX = pointX - (diamond.x + diamond.width / 2);
+    const deltaY = pointY - (diamond.y + diamond.height / 2);
+    return Math.abs(deltaX / (diamond.width / 2)) + Math.abs(deltaY / (diamond.height / 2)) <= 1;
+  };
 
   const addShape = (type) => {
     const newShape = {
@@ -120,8 +316,8 @@ const CanvasComponent = () => {
       size: 80,
     };
     setShapes([...shapes, newShape]);
+    setSelectedShape(newShape.id);
   };
-
   const getClickedShapeDouble = (x, y) => {
     return shapes.find((shape) => {
       if (shape.type === ShapeTypes.RECTANGLE) {
@@ -180,11 +376,45 @@ const CanvasComponent = () => {
     });
   };
 
+  const handleUndo = () => {};
+  const handleRedo = () => {};
+  const handleDelete = () => {
+    if (selectedShapeId) {
+      setShapes(shapes.filter((shape) => shape.id !== selectedShapeId));
+      setSelectedShapeId(null); 
+    }
+  };
+  const handleSave = () => {};
+  const handleOpen = () => {};
+
+  const handleButtonClick = (button) => {
+    setSelectedButton(button);
+    switch (button) {
+      case "open":
+        handleOpen();
+        break;
+      case "undo":
+        handleUndo();
+        break;
+      case "redo":
+        handleRedo();
+        break;
+      case "delete":
+        handleDelete();
+        break;
+      case "save":
+        handleSave();
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="dashboard-container">
       <div className="sidebar">
         <h2>Shapes</h2>
-        <div>
+        <div className="shapebutton-container">
           <button data-testid="rectangleButton" onClick={() => addShape(ShapeTypes.RECTANGLE)}>
             <Rectangle width={100} height={60} />
           </button>
@@ -201,21 +431,61 @@ const CanvasComponent = () => {
       </div>
       <div className="main">
         <div className="button-container">
-          {/* Buttons for undo, redo, delete, save, open */}
+          <button data-testid="openButton"
+            onClick={() => handleButtonClick("open")}
+            className={selectedButton === "open" ? "selected" : ""}
+          >
+            <MdFileOpen />
+            {hoveredButton === "open" && <span className="tooltip">Open</span>}
+          </button>
+          <button data-testid="saveButton"
+            onClick={() => handleButtonClick("save")}
+            className={selectedButton === "save" ? "selected" : ""}
+          >
+            <TfiSave />
+            {hoveredButton === "save" && <span className="tooltip">Save</span>}
+          </button>
+          <button data-testid="undoButton1"
+            onClick={() => handleButtonClick("undo")}
+            className={selectedButton === "undo" ? "selected" : ""}
+          >
+            <IoArrowUndo />
+            {hoveredButton === "undo" && <span className="tooltip">Undo</span>}
+          </button>
+          <button data-testid="redoButton"
+            onClick={() => handleButtonClick("redo")}
+            className={selectedButton === "redo" ? "selected" : ""}
+          >
+            <IoArrowRedo />
+            {hoveredButton === "redo" && <span className="tooltip">Redo</span>}
+          </button>
+          <button data-testid="deleteButton"
+            onClick={() => handleButtonClick("delete")}
+            className={selectedButton === "delete" ? "selected" : ""}
+          >
+            <MdDeleteForever />
+            {hoveredButton === "delete" && (
+              <span className="tooltip">Delete</span>
+            )}
+          </button>
         </div>
         <div>
           <h1>Draw Here!!</h1>
           <div style={{ position: "relative", width: "800px", height: "600px" }}>
-            <canvas
-              data-testid="canvas"
-              ref={canvasRef}
-              aria-label="Canvas"
-              width={800}
-              height={600}
-              style={{ border: "1px solid black" }}
-              onDoubleClick={handleDoubleClick}
-            ></canvas>
-            {editingShapeId && (
+          <canvas
+            data-testid="canvas"
+            ref={canvasRef}
+            aria-label="Canvas"
+            width={800}
+            height={600}
+            style={{ border: "1px solid black" }}
+            onDoubleClick={handleDoubleClick}
+            onClick={handleCanvasClick}
+            onMouseDown={handleCanvasMouseDown}
+            onMouseMove={handleCanvasMouseMove}
+            onMouseUp={handleCanvasMouseUp}
+          ></canvas>
+          {editingShapeId && (
               <input
                 data-testid="editingtextinput"
                 type="text"
@@ -233,10 +503,9 @@ const CanvasComponent = () => {
             )}
           </div>
         </div>
-
-      </div>
+        </div>
     </div>
   );
-};
 
+}
 export default CanvasComponent;
--- diagramming-tool/src/Component/Dashboard/Canvas.test.jsx
+++ diagramming-tool/src/Component/Dashboard/Canvas.test.jsx
@@ -1,6 +1,9 @@
+import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import CanvasComponent from "./Canvas";
 import { setupJestCanvasMock } from "jest-canvas-mock";
+import ShapeTypes from './ShapeTypes';
+
 
 
 
@@ -10,13 +13,48 @@ beforeEach(() => {
 });
 
 describe("Home Page", () => {
+  it("Checks Open Button Exist in Home Page", () => {
+    render(<CanvasComponent />);
+    const openButton = screen.getByTestId(/openButton/i);
+    expect(openButton).toBeInTheDocument();
+  });
+
+  it("Checks Save Button Exist in Home Page", () => {
+    render(<CanvasComponent />);
+    const saveButton = screen.getByTestId(/saveButton/i);
+    expect(saveButton).toBeInTheDocument();
+  });
+
+  it("Checks Undo Button Exist in Home Page", () => {
+    render(<CanvasComponent />);
+    const undoButton = screen.getByTestId(/undoButton/i);
+    expect(undoButton).toBeInTheDocument();
+  });
+
+  it("Checks Redo Button Exist in Home Page", () => {
+    render(<CanvasComponent />);
+    const redoButton = screen.getByTestId(/redoButton/i);
+    expect(redoButton).toBeInTheDocument();
+  });
+
+  it("Checks Delete Button Exist in Home Page", () => {
+    render(<CanvasComponent />);
+    const deleteButton = screen.getByTestId(/deleteButton/i);
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  it("Checks Page contain shapes heading", () => {
+    render(<CanvasComponent />);
+    const element = screen.getByText(/Shapes/i);
+    expect(element).toBeInTheDocument();
+  });
+
   it("Rectangle Button Exist in Home Page", () => {
     render(<CanvasComponent />);
     const rect = screen.getByTestId(/rectangleButton/i);
     expect(rect).toBeInTheDocument();
   });
-
-  it("Circle button Exist in Home Page", () => {
+  it(" Circle button Exist in Home Page", () => {
     render(<CanvasComponent />);
     const circ = screen.getByTestId(/circleButton/i);
     expect(circ).toBeInTheDocument();
@@ -33,7 +71,6 @@ describe("Home Page", () => {
     const dmnd = screen.getByTestId(/diamondButton/i);
     expect(dmnd).toBeInTheDocument();
   });
-
   it("Canvas Exist in Home Page", () => {
     render(<CanvasComponent />);
     const canvas = screen.getByTestId("canvas");
@@ -42,6 +79,36 @@ describe("Home Page", () => {
 });
 
 describe("Button Working", () => {
+  it("Clicks on Save Button", () => {
+    render(<CanvasComponent />);
+    const saveButton = screen.getByTestId(/saveButton/i);
+    fireEvent.click(saveButton);
+  });
+
+  it("Clicks on Undo Button", () => {
+    render(<CanvasComponent />);
+    const undoButton = screen.getByTestId(/undoButton/i);
+    fireEvent.click(undoButton);
+  });
+
+  it("Clicks on Redo Button", () => {
+    render(<CanvasComponent />);
+    const redoButton = screen.getByTestId(/redoButton/i);
+    fireEvent.click(redoButton);
+  });
+
+  it("Clicks on Delete Button", () => {
+    render(<CanvasComponent />);
+    const deleteButton = screen.getByTestId(/deleteButton/i);
+    fireEvent.click(deleteButton);
+  });
+
+  it("Clicks on Open Button", () => {
+    render(<CanvasComponent />);
+    const openButton = screen.getByTestId(/openButton/i);
+    fireEvent.click(openButton);
+  });
+
   it("Drawing Rectangle in Home Page", () => {
     render(<CanvasComponent />);
     const canvas = screen.getByTestId("canvas");
@@ -53,6 +120,7 @@ describe("Button Working", () => {
     expect(ctx.fillRect).toHaveBeenCalledWith(100, 100, 200, 100);
     expect(ctx.strokeRect).toHaveBeenCalledWith(100, 100, 200, 100);
   });
+  
 
   it("Drawing Circle in Home Page", () => {
     render(<CanvasComponent />);
@@ -67,24 +135,28 @@ describe("Button Working", () => {
     expect(ctx.stroke).toHaveBeenCalled();
   });
 
-  it("Drawing Square in Home Page", () => {
+  test("Drawing Square in Home Page", () => {
     render(<CanvasComponent />);
     const canvas = screen.getByTestId("canvas");
     expect(canvas).toBeInTheDocument();
+
     const squareButton = screen.getByTestId(/squareButton/i);
     fireEvent.click(squareButton);
+
     const ctx = canvas.getContext("2d");
     expect(ctx).toBeDefined();
     expect(ctx.fillRect).toHaveBeenCalledWith(100, 100, 80, 80);
     expect(ctx.strokeRect).toHaveBeenCalledWith(100, 100, 80, 80);
   });
 
-  it("Drawing Diamond in Home Page", () => {
+  test("Drawing Diamond in Home Page", () => {
     render(<CanvasComponent />);
     const canvas = screen.getByTestId("canvas");
     expect(canvas).toBeInTheDocument();
+
     const diamondButton = screen.getByTestId(/diamondButton/i);
     fireEvent.click(diamondButton);
+
     const ctx = canvas.getContext("2d");
     expect(ctx).toBeDefined();
     expect(ctx.beginPath).toHaveBeenCalled();
@@ -93,11 +165,11 @@ describe("Button Working", () => {
     expect(ctx.lineTo).toHaveBeenCalledWith(150, 200);
     expect(ctx.lineTo).toHaveBeenCalledWith(100, 150);
     expect(ctx.closePath).toHaveBeenCalled();
+
     expect(ctx.fill).toHaveBeenCalled();
     expect(ctx.stroke).toHaveBeenCalled();
   });
-});
-
+ });
 describe("Home component", () => {
   it("Draws shapes on the canvas", () => {
     render(<CanvasComponent />);
@@ -108,13 +180,29 @@ describe("Home component", () => {
   });
 });
 
-describe("CanvasComponent", () => {
+
+describe("Canvas Component", () => {
+
+  test('deleting a shape', () => {
+    render(<CanvasComponent />);
+    const rectangleButton =screen.getByTestId('rectangleButton');
+    fireEvent.click(rectangleButton);
+
+    
+    const deleteButton = screen.getByTestId('deleteButton');
+    fireEvent.click(deleteButton);
+
+   
+    const canvas = screen.getByTestId('canvas');
+    const ctx = canvas.getContext('2d');
+    expect(ctx).toBeDefined();
+    expect(ctx.clearRect).toHaveBeenCalled(); 
+  });
   it("handles double-click events", () => {
     render(<CanvasComponent />);
     const canvas = screen.getByTestId('canvas');
     fireEvent.dblClick(canvas);
   });
-
   it("displays text input for rectangle shape on double click", () => {
     render(<CanvasComponent />);
     const canvas = screen.getByTestId('canvas');
@@ -135,8 +223,8 @@ describe("CanvasComponent", () => {
     const circleButton = screen.getByTestId('circleButton');
     fireEvent.click(circleButton);
     const rect = canvas.getBoundingClientRect();
-    const centerX = 200; 
-    const centerY = 200; 
+    const centerX = 100; 
+    const centerY = 100; 
     const radius = 50;   
     fireEvent.doubleClick(canvas, {
       clientX: rect.left + centerX,
@@ -189,8 +277,82 @@ describe("CanvasComponent", () => {
     fireEvent.change(textInput, { target: { value: 'New Text' } });
     expect(textInput.value).toBe('New Text');
   });
+ 
+  it("selects a shape on canvas click", () => {
+    render(<CanvasComponent />);
+    const canvas = screen.getByTestId("canvas");
+    const rectangleButton = screen.getByTestId("rectangleButton");
+
+    fireEvent.click(rectangleButton); 
+
+    fireEvent.click(canvas, { nativeEvent: { offsetX: 110, offsetY: 110 } }); 
+    const selectedRectangle = screen.getByTestId("rectangleButton");
+    expect(selectedRectangle).toBeInTheDocument();
+  });
+
+  
+    it("selects a circle on canvas click", () => {
+      render(<CanvasComponent />);
+      const canvas = screen.getByTestId("canvas");
+      const circleButton = screen.getByTestId("circleButton");
+   
+      fireEvent.click(circleButton); 
+   
+      fireEvent.click(canvas, { nativeEvent: { offsetX: 110, offsetY: 110 } }); 
+      const selectedCircle = screen.getByTestId("circleButton");
+      expect(selectedCircle).toBeInTheDocument();
+    });
+    it("selects a square on canvas click", () => {
+      render(<CanvasComponent />);
+      const canvas = screen.getByTestId("canvas");
+      const squareButton = screen.getByTestId("squareButton");
+   
+      fireEvent.click(squareButton); 
+   
+      fireEvent.click(canvas, { nativeEvent: { offsetX: 110, offsetY: 110 } }); 
+      const selectedSquare = screen.getByTestId("squareButton");
+      expect(selectedSquare).toBeInTheDocument();
+    });
+    it("selects a diamond on canvas click", () => {
+      render(<CanvasComponent />);
+      const canvas = screen.getByTestId("canvas");
+      const diamondButton = screen.getByTestId("diamondButton");
+   
+      fireEvent.click(diamondButton); 
+   
+      fireEvent.click(canvas, { nativeEvent: { offsetX: 110, offsetY: 110 } }); 
+      const selectedDiamond = screen.getByTestId("diamondButton");
+      expect(selectedDiamond).toBeInTheDocument();
+    });
+   
+
+
+  it("Does not draw a line when the mouse is not dragged", () => {
+    render(<CanvasComponent />);
+    const canvas = screen.getByTestId("canvas");
+    expect(canvas).toBeInTheDocument();
+
+    fireEvent.mouseDown(canvas, { nativeEvent: { offsetX: 100, offsetY: 100 } });
+    fireEvent.mouseUp(canvas, { nativeEvent: { offsetX: 100, offsetY: 100 } });
+
+    const context = canvas.getContext("2d");
+    expect(context).toBeDefined();
+    expect(context.beginPath).not.toHaveBeenCalled();
+    expect(context.moveTo).not.toHaveBeenCalled();
+    expect(context.lineTo).not.toHaveBeenCalled();
+    expect(context.stroke).not.toHaveBeenCalled();
+  });
+  test('dragging a line', () => {
+    render(<CanvasComponent />);
+    const canvas = screen.getByTestId('canvas');
+    fireEvent.mouseDown(canvas, { nativeEvent: { offsetX: 100, offsetY: 100 } });
+    fireEvent.mouseMove(canvas, { nativeEvent: { offsetX: 200, offsetY: 200 } });
+  
+    fireEvent.mouseUp(canvas);
+  
+    const canvasContext = canvas.getContext('2d');
+    expect(canvasContext).toBeDefined();
+    expect(canvasContext.beginPath).toHaveBeenCalledTimes(0);
+    expect(canvasContext.stroke).toHaveBeenCalledTimes(0);
+  });
 });
-
-
-
-
